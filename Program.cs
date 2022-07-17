dc1fb89138af3b5a0be3eca0acd1a5d4d4f959b6
Madina <madinarn@code.edu.az>
2022-07-18 00:51:45 +0400

    no commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace ConsoleApp12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            User user1 = new User("Madina", "Novruzova", "S4FL", User.RegistrationMonth.January);
+            User user2 = new User("Aysel", "Orucova", "D2NB", User.RegistrationMonth.October);
+            User user3 = new User("Huseyn", "Ahmedov", "AFD3", User.RegistrationMonth.March);
+            User user4 = new User("Aysu", "Aliyeva", "H2FB", User.RegistrationMonth.November);
+            User user5 = new User("Murad", "Agayev", "SJ5X", User.RegistrationMonth.December);
+
+            Console.WriteLine(user1.GetDetails());
+            Console.WriteLine(user2.GetDetails());
+            Console.WriteLine(user3.GetDetails());
+            Console.WriteLine(user4.GetDetails());
+            Console.WriteLine(user5.GetDetails());
+            
+        }
+    }
+}
---
+++ User.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp12
+{
+     public class User
+    {
+        private static int id = 1;
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int Id { get; set; }
+        public string NotRobotCode { get; set; }
+       
+        public enum RegistrationMonth { January,February,March,April,May,June,July,August,September,October,November,December}
+        public RegistrationMonth Month { get; set; }
+        public User(string name, string surname, string notRobotCode,RegistrationMonth month)
+        {
+            Name = name;
+            Surname = surname;
+            Id = id++;
+            NotRobotCode = notRobotCode;
+            Month = month;
+        }
+        public (int,string,string,RegistrationMonth) GetDetails()
+
+    {
+            if (Extension.CheckNotRobotCode(NotRobotCode))
+            {
+                return (Id, Surname, Name, Month);
+            }
+            else
+            {
+                Console.WriteLine("not correct");
+                {
+                    return (Id, Surname, Name, Month);
+                }
+            }
+
+    }
+  }
+}
